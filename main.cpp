1219aa64522b7e5c249185a5d1cd8b83c5d79097
HamamatuReiya <k022g0050@g.neec.ac.jp>
2024-11-26 15:35:10 +0900

    02_04途中

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cassert>
+#include "TextureConverter.h"
+
+enum Argument{
+	kApplicationPath,  // アプリケーションのパス
+	kFilePath,         // 渡されたファイルパス
+
+	NumArgument
+};
 
 int main(int argc, char* argv[])
 {
-	for (int i = 0; i < argc; i++)
-	{
-		printf(argv[i]);
-		printf("\n");
-	}
+	assert(argc >= NumArgument);
+
+	// COM ライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINITBASE_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	// テクスチャコンバータ
+	TextureConverter converter;
+
+	// テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	// COM ライブラリの終了
+	CoUninitialize();
 
 	system("pause");
 	return 0;
